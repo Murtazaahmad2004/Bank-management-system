cc27d5fe27f8c4df740bf3ae0c7eb9136d764ef2
Murtaza Ahmad <shmurtazaahmad336@gmail.com>
2025-02-21 19:34:30 +0500

    commit this

--- python.py
+++ python.py
@@ -34,6 +34,10 @@ class Admin(User):
 class Employee(User):
     def __init__(self, userid, password):
         super().__init__(userid, password)
+        
+class Customer(User):
+    def __init__(self, userid, password):
+        super().__init__(userid, password)
 
 # Routes
 
@@ -53,9 +57,34 @@ def logout():
     return redirect(url_for('home'))
 
 # Account Management Page
-@app.route('/account_manage_entity')
+@app.route('/emp_dashboard/account_manage_entity')
 def account_manage_entity():
-    return render_template('account_manage_entity.html')
+    return render_template('/emp_dashboard/account_manage_entity.html')
+
+#open acc
+@app.route('/emp_dashboard/open_digital_account')
+def open_digital_account():
+    return render_template('emp_dashboard/open_digital_account.html')
+
+# blocked acc
+@app.route('/emp_dashboard/blocked_acc')
+def blocked_accounts():
+    return render_template('/emp_dashboard/blocked_acc.html')
+
+# crad management
+@app.route('/emp_dashboard/card_management')
+def card_management():
+    return render_template('/emp_dashboard/card_management.html')
+
+#chequebook 
+@app.route('/emp_dashboard/chequebook_management')
+def chequebook_management():
+    return render_template('emp_dashboard/chequebook_management.html')
+
+#Deposite
+@app.route('/emp_dashboard/deposit_money')
+def deposit_money():
+    return render_template('emp_dashboard/deposit_money.html')
 
 # Login
 @app.route('/login_user_employee_auth', methods=['POST'])
@@ -68,31 +97,6 @@ def login_user_employee_auth():
         return redirect(url_for('account_manage_entity'))  # Redirect if credentials are correct
     else:
         return "Invalid credentials. Please try again."
-
-#open acc
-@app.route('/open_digital_account')
-def open_digital_account():
-    return render_template('open_digital_account.html')
-
-# blocked acc
-@app.route('/blocked_acc')
-def blocked_accounts():
-    return render_template('blocked_acc.html')
-
-# crad management
-@app.route('/card_management')
-def card_management():
-    return render_template('card_management.html')
-
-#chequebook 
-@app.route('/chequebook_management')
-def chequebook_management():
-    return render_template('chequebook_management.html')
-
-#Deposite
-@app.route('/deposit_money')
-def deposit_money():
-    return render_template('deposit_money.html')
 
 # Run the app
 if __name__ == '__main__':
